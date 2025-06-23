a6196382b6f716873db90eda632f1d5e2b2a8205
Rishi-314 <rishikesh2048x@gmail.com>
2025-06-23 18:50:14 +0530

    1_madlib project

---
+++ 1_madlib.py
@@ -0,0 +1,26 @@
+# string concatenation 
+# how to put strings together 
+
+
+name = input("Enter the name ")
+
+print("The name is " + name)
+
+print("The name is {}".format(name))
+
+print(f"The name is {name}")
+
+print("\n")
+
+print("Now we will do a longer version of this!! \nSo we will create a youtube introduction for different creaters")
+
+print("\n")
+
+youtubername = input("Enter the name of youtuber: ")
+video_title = input("Enter the title of video: ")
+
+print("\n")
+
+print(f"Hello guys! Welcome back to my channel {youtubername}, \n\nSo today's video is about {video_title} \n\nBut before we begin our video, please it like and subscribe to our channel {youtubername}")
+
+print("\n")
